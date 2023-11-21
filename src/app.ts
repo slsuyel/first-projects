a6d0d74b2ef4f8e37e9df6f6e54b529e8c71926e
Suyel Haque <slsuyel@gmail.com>
2023-11-21 20:34:55 +0600

    2nd day

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
+
+const app: Application = express();
+
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  res.send('Hello world');
+};
+
+app.get('/', getAController);
+
+export default app;
